07330164fec2145e4ee977188aff3cc32d5b17e1
Guard <guardweicheng@163.com>
2019-06-06 10:30:45 +0800

    删除部分SPFileUtil方法 新增 SPFileUtil 不用返回值方法

--- commonlib/src/main/java/cn/vacuumflask/commonlib/SPFileUtil.java
+++ commonlib/src/main/java/cn/vacuumflask/commonlib/SPFileUtil.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -16,124 +17,6 @@ import java.util.TreeMap;
  * SharedPreferences 工具类
  */
 public class SPFileUtil {
-    private String fileName;
-    private Context context;
-    private SharedPreferences sharedPreferences;
-
-    public SPFileUtil(String fileName, Context context) {
-        this.fileName = fileName;
-        this.context = context;
-        sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    /**
-     * 拿到相对应的键的值
-     *
-     * @param key 键值
-     * @return 返回相对键值的数据
-     */
-    public String getFileMessage(String key) {
-        return sharedPreferences.getString(key, "");
-    }
-
-    /**
-     * 返回SharedPreference文件中的所有键值
-     *
-     * @return 键值的集合
-     */
-    public List<String> getKeys() {
-        Map<String, ?> map = sharedPreferences.getAll();
-        Set<? extends Map.Entry<String, ?>> entries = map.entrySet();
-        Iterator<? extends Map.Entry<String, ?>> iterator = entries.iterator();
-        ArrayList<String> keys = new ArrayList<>();
-        while (iterator.hasNext()) {
-            Map.Entry<String, ?> entry = iterator.next();
-            String key = entry.getKey();
-            keys.add(key);
-        }
-        return keys;
-    }
-
-    /**
-     * 返回SharedPreference文件中是否含有该值
-     *
-     * @return 键值的集合
-     */
-    public boolean hasKey(String key) {
-        return sharedPreferences.contains(key);
-    }
-
-    /**
-     * @param key     键值
-     * @param defalut 默认值
-     * @return
-     */
-    public boolean getFileBoolean(String key, boolean defalut) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
-        return sharedPreferences.getBoolean(key, defalut);
-    }
-
-    /**
-     * 保存先对应的Key值和Value值
-     *
-     * @param key   键值
-     * @param value 键值相对应的值
-     * @return 返回boolean
-     */
-    public boolean setSPFileValue(String key, String value) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(key, value);
-        return editor.commit();
-    }
-
-    /**
-     * 保存先对应的Key值和Value值
-     *
-     * @param key   键值
-     * @param value 键值相对应的值
-     * @return 返回boolean
-     */
-    public boolean setSPFileValue(String key, boolean value) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(key, value);
-        return editor.commit();
-    }
-
-    /**
-     * 保存先对应的Key值和Value值
-     *
-     * @param key   键值
-     * @param value 键值相对应的值
-     * @return 返回boolean
-     */
-    public boolean setSPFileValue(String key, int value) {
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putInt(key, value);
-        return editor.commit();
-    }
-
-    /**
-     * 删除所有数据
-     *
-     * @return 返回是否成功
-     */
-    public boolean deleteAllMessage() {
-        return sharedPreferences.edit().clear().commit();
-    }
-
-    /**
-     * 删除指定的键值所对应的值
-     *
-     * @param key 键值
-     * @return 返回是否成功
-     */
-    public boolean deleteSingle(String key) {
-        return sharedPreferences.edit().remove(key).commit();
-    }
 
     /**
      * 不实例化对象 获取文件中相对应的值
@@ -305,6 +188,77 @@ public class SPFileUtil {
     public static boolean setMessage(Context context, String fileName, String key, long vlaue) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
         return sharedPreferences.edit().putLong(key, vlaue).commit();
+    }
+
+    /**
+     * 不实例化对象，直接保存数据，且不返回结果
+     *
+     * @param context  上下文
+     * @param fileName 文件名
+     * @param key      键值
+     * @param vlaue    值
+     */
+    public static void setMessageApply(Context context, String fileName, String key, String vlaue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString(key, vlaue).apply();
+    }
+
+    /**
+     * 不实例化对象，直接保存数据，且不返回结果
+     *
+     * @param context  上下文
+     * @param fileName 文件名
+     * @param key      键值
+     * @param vlaue    值
+     */
+    public static void setMessageApply(Context context, String fileName, String key, boolean vlaue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putBoolean(key, vlaue).apply();
+    }
+
+    /**
+     * 不实例化对象，直接保存数据，且不返回结果
+     *
+     * @param context  上下文
+     * @param fileName 文件名
+     * @param key      键值
+     * @param vlaue    值
+     */
+    public static void setMessageApply(Context context, String fileName, String key, int vlaue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putInt(key, vlaue).apply();
+    }
+
+    /**
+     * 不实例化对象，直接保存数据，且不返回结果
+     *
+     * @param context  上下文
+     * @param fileName 文件名
+     * @param key      键值
+     * @param vlaue    值
+     */
+    public static void setMessageApply(Context context, String fileName, String key, long vlaue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        sharedPreferences.edit().putLong(key, vlaue).apply();
+    }
+
+    /**
+     * 不实例化对象，批量保存数据，且不返回结果
+     *
+     * @param context  上下文
+     * @param fileName 文件名
+     * @param map    值
+     */
+    public static void setMessageApply(Context context, String fileName, HashMap<String, String> map) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
+        for (Map.Entry<String, String> entry : entrySet) {
+            String entryKey = entry.getKey();
+            String entryValue = entry.getValue();
+            editor.putString(entryKey, entryValue);
+        }
+        editor.apply();
     }
 
     /**
